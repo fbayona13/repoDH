6f619c706f1f66636ddaf609ee9498041ee165e6
fbayona13 <fbayona13@gmail.com>
2022-05-12 13:19:58 -0300

    sin cambios

--- clase0305/teorico.js
+++ clase0305/teorico.js
@@ -29,11 +29,11 @@
   
 /*************************************************************************** */
 
-let estudiantes = [
-    {nombre: 'John', promedio: 8.5, aprobado: true},
-    {nombre: 'Jane', promedio: 7, aprobado: true},
-    {nombre: 'June', promedio: 3, aprobado: false},
-]
+// let estudiantes = [
+//     {nombre: 'John', promedio: 8.5, aprobado: true},
+//     {nombre: 'Jane', promedio: 7, aprobado: true},
+//     {nombre: 'June', promedio: 3, aprobado: false},
+// ]
 
 // let aprobados = estudiantes.filter(estudiantes => estudiantes.aprobado == true);
 
