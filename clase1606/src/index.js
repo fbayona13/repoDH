11306bd677233493ddd0327e57ff034c0bbbf8c7
fbayona13 <fbayona13@gmail.com>
2022-06-15 11:28:01 -0300

    ejemplo de mercadoLiebre

--- clase1606/src/index.js
+++ clase1606/src/index.js
@@ -1,11 +1,20 @@
 let express = require("express");
 let app = express();
 let path = require("path");
+const port = process.env.PORT || 2020;
+const callback = () => console.log('server started at localhost:' + port);
+
+app.listen(port, callback);
 
 const public = path.resolve(__dirname, "../public");
 app.use(express.static(public));
 
-app.listen(3000, () => console.log("Esto fue existoso"));
 app.get("/", (req, res) =>
   res.sendFile(path.resolve(__dirname, "./views/home.html"))
+);
+app.get("/login", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "./views/login.html"))
+);
+app.get("/register", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "./views/register.html"))
 );
